e507dcae6c324fcf68dfb42f69814f010c54ca93
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-29 15:32:20 +0530

    prepare for deployment.

---
+++ src/data/certifications.js
@@ -0,0 +1,20 @@
+const certifications = [
+  {
+    id: 1,
+    title: "AWS Certified Cloud Practitioner",
+    issuer: "Amazon Web Services",
+    year: "2025",
+    image: "",
+    credential: "#"
+  },
+  {
+    id: 2,
+    title: "Introduction to Internet of Things",
+    issuer: "NPTEL",
+    year: "2025",
+    image: "",
+    credential: "#"
+  }
+];
+
+export default certifications;
