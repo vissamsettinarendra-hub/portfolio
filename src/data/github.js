78b626678f8ab2d9a0cf83c9550143c6eafc1e24
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-29 15:29:42 +0530

    prepare for deployment.

---
+++ src/data/github.js
@@ -0,0 +1,30 @@
+const repositories = [
+  {
+    id: 1,
+    name: "CropBridge",
+    description:
+      "A MERN Stack platform connecting farmers directly with factories.",
+    tech: "React • Node.js • Express • MongoDB • AWS",
+    github: "https://github.com/vissamsettinarendra-hub/CropBridge"
+  },
+
+  {
+    id: 2,
+    name: "Placement Management System",
+    description:
+      "Placement portal for students, companies and administrators.",
+    tech: "React • Node.js • Express • MongoDB",
+    github: "https://github.com/vissamsettinarendra-hub/placementproject"
+  },
+
+  {
+    id: 3,
+    name: "Portfolio Website",
+    description:
+      "Personal portfolio developed using React and modern UI design.",
+    tech: "React • CSS • JavaScript",
+    github: "https://github.com/vissamsettinarendra-hub"
+  }
+];
+
+export default repositories;
