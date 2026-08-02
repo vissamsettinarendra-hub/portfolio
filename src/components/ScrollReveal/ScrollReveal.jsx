fb5aa3feab7262be8b445bf6d57660ab2671760e
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-08-02 21:58:23 +0530

    prepare for deployment

---
+++ src/components/ScrollReveal/ScrollReveal.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+function ScrollReveal({ children }) {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: 80,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      viewport={{
+        once: true,
+        amount: 0.2,
+      }}
+      transition={{
+        duration: 0.8,
+        ease: "easeOut",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default ScrollReveal;
